3268130c145c9409b580aa8a4d944128fe0e63fb
Yaro Shm <yshmarov@gmail.com>
2025-02-25 00:57:50 +0100

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,42 @@
 product_objects = [
-  { name: 'Prince Polo', price: 199, category: 'food', image_url: 'https://i.imgur.com/l4tEqmL.png', description: 'Classic wafer bar covered in dark chocolate' },
-  { name: 'Donut Chocolate', price: 129, category: 'food', image_url: 'https://i.imgur.com/LRwEakM.png' },
-  { name: 'Donut limited', price: 159, category: 'food', image_url: 'https://i.imgur.com/3sgNWeJ.png' },
-  { name: 'Banana', price: 99, category: 'food', image_url: 'https://i.imgur.com/eUzrUjG.png' },
-  { name: 'Mullermilch CoCo', price: 499, category: 'drinks', image_url: 'https://i.imgur.com/OGLkpV5.png' },
-  { name: 'Mullermilch Chocolate', price: 499, category: 'drinks', image_url: 'https://i.imgur.com/fCHrRRT.png' },
-  { name: 'Mullermilch Strawberry', price: 499, category: 'drinks', image_url: 'https://i.imgur.com/0HICucM.png' },
-  { name: 'Monster classic', price: 599, category: 'drinks', image_url: 'https://i.imgur.com/WEDXcDi.png', description: 'Original energy drink with a powerful blend of caffeine and B-vitamins' },
-  { name: 'Monster juiced', price: 599, category: 'drinks', image_url: 'https://i.imgur.com/QNLaQ9L.png', description: 'Energizing blend with real fruit juice' },
-  { name: 'Polaris Gazowana', price: 199, category: 'drinks', image_url: 'https://i.imgur.com/7BGt24V.png', description: 'Refreshing sparkling mineral water' },
-  { name: 'Polaris Niegazowana', price: 159, category: 'drinks', image_url: 'https://i.imgur.com/CkaVyAQ.png', description: 'Pure still mineral water' },
-  { name: 'Sezamki Classic', price: 399, category: 'food', image_url: 'https://i.imgur.com/r1pZQ1P.png', description: 'Traditional sesame seed candy bars' },
-  { name: 'Rafaello', price: 99, category: 'food', image_url: 'https://i.imgur.com/XwLtkud.png', description: 'Coconut-almond confection in crispy white shell' },
-  { name: 'Mini Bounty', price: 59, category: 'food', image_url: 'https://i.imgur.com/JasXScr.png', description: 'Mini chocolate bars with coconut filling' },
-  { name: 'Mini Snickers', price: 59, category: 'food', image_url: 'https://i.imgur.com/ORYBJqE.png', description: 'Mini chocolate bars with peanuts, caramel and nougat' }
+  { name: 'Prince Polo', price: 199, image_url: 'https://i.imgur.com/l4tEqmL.png', description: 'Classic wafer bar covered in dark chocolate' },
+  { name: 'Donut Chocolate', price: 129, image_url: 'https://i.imgur.com/LRwEakM.png' },
+  { name: 'Donut limited', price: 159, image_url: 'https://i.imgur.com/3sgNWeJ.png' },
+  { name: 'Banana', price: 99, image_url: 'https://i.imgur.com/eUzrUjG.png' },
+  { name: 'Mullermilch CoCo', price: 499, image_url: 'https://i.imgur.com/OGLkpV5.png' },
+  { name: 'Mullermilch Chocolate', price: 499, image_url: 'https://i.imgur.com/fCHrRRT.png' },
+  { name: 'Mullermilch Strawberry', price: 499, image_url: 'https://i.imgur.com/0HICucM.png' },
+  { name: 'Monster classic', price: 599, image_url: 'https://i.imgur.com/WEDXcDi.png', description: 'Original energy drink with a powerful blend of caffeine and B-vitamins' },
+  { name: 'Monster juiced', price: 599, image_url: 'https://i.imgur.com/QNLaQ9L.png', description: 'Energizing blend with real fruit juice' },
+  { name: 'Polaris Gazowana', price: 199, image_url: 'https://i.imgur.com/7BGt24V.png', description: 'Refreshing sparkling mineral water' },
+  { name: 'Polaris Niegazowana', price: 159, image_url: 'https://i.imgur.com/CkaVyAQ.png', description: 'Pure still mineral water' },
+  { name: 'Sezamki Classic', price: 399, image_url: 'https://i.imgur.com/r1pZQ1P.png', description: 'Traditional sesame seed candy bars' },
+  { name: 'Rafaello', price: 99, image_url: 'https://i.imgur.com/XwLtkud.png', description: 'Coconut-almond confection in crispy white shell' },
+  { name: 'Mini Bounty', price: 59, image_url: 'https://i.imgur.com/JasXScr.png', description: 'Mini chocolate bars with coconut filling' },
+  { name: 'Mini Snickers', price: 59, image_url: 'https://i.imgur.com/ORYBJqE.png', description: 'Mini chocolate bars with peanuts, caramel and nougat' }
 ]
 
-product_objects.each do |product_object|
-  stripe_product = Stripe::Product.create(name: product_object[:name], images: [ product_object[:image_url] ], description: product_object[:description])
-  stripe_price = Stripe::Price.create(product: stripe_product.id, unit_amount: product_object[:price], currency: Setting.currency)
-  product = Product.find_or_initialize_by(product_object)
-  product.stripe_price_id = stripe_price.id
-  product.save!
+if Stripe::Product.list.empty?
+  product_objects.each do |product_object|
+    stripe_product = Stripe::Product.create(name: product_object[:name], images: [ product_object[:image_url] ], description: product_object[:description])
+    stripe_price = Stripe::Price.create(product: stripe_product.id, unit_amount: product_object[:price], currency: Setting.currency)
+    stripe_product.default_price = stripe_price.id
+    stripe_product.save
+
+    product = Product.find_or_initialize_by(name: product_object[:name], price: product_object[:price])
+    product.stripe_product = Stripe::Product.retrieve(stripe_product.id)
+    product.save!
+  end
+else
+  Product.destroy_all
+
+  stripe_products = Stripe::Product.list
+  stripe_products.each do |stripe_product|
+    if stripe_product.default_price.present?
+      stripe_price = Stripe::Price.retrieve(stripe_product.default_price)
+      product = Product.find_or_initialize_by(name: stripe_product.name, price: stripe_price.unit_amount)
+      product.stripe_product = stripe_product
+      product.save!
+    end
+  end
 end
