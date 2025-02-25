d171b166743c2fb9fae5d7be1ec2f0246ae75759
Yaro Shm <yshmarov@gmail.com>
2025-02-25 11:53:40 +0100

    display product marketing features

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -2,4 +2,9 @@ module ProductHelper
   def price_label(product)
     number_to_currency(product.default_price / 100.0, unit: product.default_currency)
   end
+
+  def marketing_features(product)
+    marketing_features = product.stripe_product["marketing_features"]
+    marketing_features.map { |feature| feature["name"] }
+  end
 end
