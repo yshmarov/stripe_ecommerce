0c51a5094a99ee253b0ffb3ec4e98faa276c8307
Yaro Shm <yshmarov@gmail.com>
2025-02-26 19:18:27 +0100

    recurring prices are not valid for ecomm store

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ApplicationRecord
   has_many :order_items, dependent: :restrict_with_error
   has_many :orders, through: :order_items
 
-  scope :with_default_price, -> { where.not(stripe_product: nil).where("stripe_product->>'default_price' IS NOT NULL") }
+  scope :with_default_price, -> { where.not(stripe_product: nil).where("stripe_product->>'default_price' IS NOT NULL").where("stripe_product->'default_price'->>'recurring' IS NULL") }
   default_scope { with_default_price }
 
   scope :search, ->(query) {
