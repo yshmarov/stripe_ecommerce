7f0d1fd7bf82becba257c87107da29abba69b56f
Yaro Shm <yshmarov@gmail.com>
2025-02-27 00:41:00 +0100

    add normal policies pages

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,13 +1,13 @@
 class StaticController < ApplicationController
   def terms_of_service
-    render plain: Setting.terms_of_service
+    render "static/index"
   end
 
   def privacy_policy
-    render plain: Setting.privacy_policy
+    render "static/index"
   end
 
   def refund_policy
-    render plain: Setting.refund_policy
+    render "static/index"
   end
 end
