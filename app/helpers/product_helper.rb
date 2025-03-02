33fe783c55d9eaceeb9bc758bc452e3e676cb188
Yaro Shm <yshmarov@gmail.com>
2025-03-02 23:50:59 +0100

    add a product schema

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -5,4 +5,23 @@ module ProductHelper
 
     marketing_features.map { |feature| feature["name"] }
   end
+
+  # https://schema.org/Product
+  def product_schema_json(product)
+    {
+      "@context": "https://schema.org",
+      "@type": "Product",
+      "name": product.name,
+      "description": product.stripe_product["description"],
+      "image": product.stripe_product["images"].first,
+      "sku": product.default_price.stripe_price_id,
+      "offers": {
+        "@type": "Offer",
+        "url": product_url(product),
+        "priceCurrency": product.default_price.stripe_price["currency"],
+        "price": product.default_price.stripe_price["unit_amount"].to_f / 100,
+        "availability": "https://schema.org/InStock"
+      }
+    }.to_json.html_safe
+  end
 end
