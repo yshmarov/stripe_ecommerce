b3879fd4c78289de99469e0e266434542b13dfbb
Yaro Shm <yshmarov@gmail.com>
2025-03-02 08:17:33 +0100

    SyncStripeProductsJob syncs prices

--- app/jobs/sync_stripe_products_job.rb
+++ app/jobs/sync_stripe_products_job.rb
@@ -9,6 +9,14 @@ class SyncStripeProductsJob < ApplicationJob
       product.name = stripe_product.name
       product.stripe_product = stripe_product
       product.save!
+
+      prices = Stripe::Price.list(product: stripe_product.id)
+      prices.each do |stripe_price|
+        price = Price.find_or_initialize_by(stripe_price_id: stripe_price.id)
+        price.product = product
+        price.stripe_price = stripe_price
+        price.save!
+      end
     end
   end
 end
