93b9ef8de64ed8304db95053a66b38fa00b2c162
Yaro Shm <yshmarov@gmail.com>
2025-03-02 13:52:21 +0100

    handle no marketing features

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -1,6 +1,8 @@
 module ProductHelper
   def marketing_features(product)
     marketing_features = product.stripe_product["marketing_features"]
+    return [] if marketing_features.blank?
+
     marketing_features.map { |feature| feature["name"] }
   end
 end
