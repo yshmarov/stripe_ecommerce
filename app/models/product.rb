e10c429333f230d515adb2b5b6cf7522313e6e20
Yaro Shm <yshmarov@gmail.com>
2025-03-06 17:09:08 +0100

    handle default_price nil

--- app/models/product.rb
+++ app/models/product.rb
@@ -27,12 +27,19 @@ class Product < ApplicationRecord
   end
 
   def default_price
-    default_price_id = if stripe_product_object.default_price.is_a?(String)
+    default_price_id = if stripe_product_object.default_price.nil?
+      nil
+    elsif stripe_product_object.default_price.is_a?(String)
       stripe_product_object.default_price
     else
       stripe_product_object.default_price.id
     end
-    prices.detect { |price| price.stripe_price_id == default_price_id } || prices.first
+
+    if default_price_id.nil?
+      prices.first
+    else
+      prices.detect { |price| price.stripe_price_id == default_price_id } || prices.first
+    end
   end
 
   def default_unit_amount
