1c075e829b4ab4ecdf7f08607fca0a3c29af270a
Yaro Shm <yshmarov@gmail.com>
2025-03-06 11:52:20 +0100

    unarchive works
    default scope was preventing

--- app/jobs/stripe/sync_product_job.rb
+++ app/jobs/stripe/sync_product_job.rb
@@ -2,7 +2,7 @@ class Stripe::SyncProductJob < ApplicationJob
   queue_as :default
 
   def perform(stripe_product_id)
-    product = Product.find_or_initialize_by(stripe_product_id: stripe_product_id)
+    product = Product.unscoped.find_or_initialize_by(stripe_product_id: stripe_product_id)
     stripe_product = Stripe::Product.retrieve(stripe_product_id)
     product.name = stripe_product.name
     product.stripe_product = stripe_product
@@ -10,7 +10,7 @@ class Stripe::SyncProductJob < ApplicationJob
 
     prices = Stripe::Price.list(product: stripe_product_id)
     prices.each do |stripe_price|
-      price = Price.find_or_initialize_by(stripe_price_id: stripe_price.id)
+      price = Price.unscoped.find_or_initialize_by(stripe_price_id: stripe_price.id)
       price.product = product
       price.stripe_price = stripe_price
       price.save!
--- app/jobs/stripe/sync_products_job.rb
+++ app/jobs/stripe/sync_products_job.rb
@@ -5,14 +5,14 @@ class Stripe::SyncProductsJob < ApplicationJob
     Stripe::Product.list(
       expand: [ "data.default_price" ]
     ).each do |stripe_product|
-      product = Product.find_or_initialize_by(stripe_product_id: stripe_product.id)
+      product = Product.unscoped.find_or_initialize_by(stripe_product_id: stripe_product.id)
       product.name = stripe_product.name
       product.stripe_product = stripe_product
       product.save!
 
       prices = Stripe::Price.list(product: stripe_product.id)
       prices.each do |stripe_price|
-        price = Price.find_or_initialize_by(stripe_price_id: stripe_price.id)
+        price = Price.unscoped.find_or_initialize_by(stripe_price_id: stripe_price.id)
         price.product = product
         price.stripe_price = stripe_price
         price.save!
