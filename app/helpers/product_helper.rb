ee43922ed49e7cf2dd32197b7d46b54a30323e71
Yaro Shm <yshmarov@gmail.com>
2025-03-08 16:43:32 +0100

    add a product schema (#43)
    * add a product schema
    
    * improve product schema

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -5,4 +5,45 @@ module ProductHelper
 
     marketing_features.map { |feature| feature["name"] }
   end
+
+  # https://schema.org/Product
+  def product_schema_json(product)
+    {
+      "@context": "https://schema.org",
+      "@type": "Product",
+      "name": product.name,
+      "description": product.stripe_product["description"],
+      "image": product.stripe_product["images"].first,
+      "url": product_url(product),
+      "sku": product.default_price.stripe_price_id,
+      # "brand": {
+      #   "@type": "Brand",
+      #   "name": product.brand_name
+      # },
+      # "category": "",
+      "offers": {
+        "@type": "Offer",
+        "price": product.default_price.stripe_price["unit_amount"].to_f / 100,
+        "priceCurrency": product.default_price.stripe_price["currency"],
+        "availability": "InStock",
+        "condition": "NewCondition"
+      }
+      # "aggregateRating": {
+      #   "@type": "AggregateRating",
+      #   "ratingValue": "5",
+      #   "ratingCount": "499"
+      # },
+      # "review": {
+      #   "@type": "Review",
+      #   "reviewRating": {
+      #     "@type": "Rating",
+      #     "ratingValue": "5"
+      #   },
+      #   "author": {
+      #     "@type": "Person",
+      #     "name": "Anonymous"
+      #   }
+      # }
+    }.to_json.html_safe
+  end
 end
