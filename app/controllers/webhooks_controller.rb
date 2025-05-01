fc2c82430e3a6714b2ab46ff2f27d1b56fb9ae2d
Yaro Shm <yshmarov@gmail.com>
2025-05-01 11:27:08 +0200

    Notifications::OrderCreatedJob

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,8 +1,4 @@
 class WebhooksController < ActionController::Base
-  include ActionView::Helpers::NumberHelper
-  include Rails.application.routes.url_helpers
-  include ActionView::Helpers::UrlHelper
-
   skip_before_action :verify_authenticity_token
 
   def create
@@ -45,7 +41,7 @@ class WebhooksController < ActionController::Base
 
       order.submitted! if order.draft?
 
-      notify_admin_channel(order)
+      Notifications::OrderCreatedJob.perform_later(order)
     end
 
     render json: { status: "success" }
@@ -74,33 +70,5 @@ class WebhooksController < ActionController::Base
     end
 
     order.calculate_total_amount
-  end
-
-  def notify_admin_channel(order)
-    message = <<~MESSAGE
-      Нове замовлення: #{order.obfuscated_id}
-      #{admin_order_url(order, host: "example.com")}
-
-      #{order.order_items.map { |item| "#{item.quantity}x #{item.price.product.name} - #{number_to_currency(item.total_amount / 100.0, unit: currency_code_to_symbol(item.price.stripe_price_object.currency))}" }.join("\n")}
-
-      Доставка: #{number_to_currency(order.stripe_checkout_session_object.total_details.amount_shipping.to_f / 100, unit: currency_code_to_symbol(order.currency))}
-      Податок: #{number_to_currency(order.stripe_checkout_session_object.total_details.amount_tax.to_f / 100, unit: currency_code_to_symbol(order.currency))}
-      Знижка: #{number_to_currency(-order.stripe_checkout_session_object.total_details.amount_discount.to_f / 100, unit: currency_code_to_symbol(order.currency))}
-      Загальна сума: #{number_to_currency(order.total_amount / 100.0, unit: currency_code_to_symbol(order.currency))}
-
-      Адреса доставки:
-      #{order.stripe_checkout_session_object.collected_information.shipping_details.address.values.compact.join(", ")}
-
-      Ім'я:
-      #{order.stripe_checkout_session_object.customer_details.name}
-
-      Email:
-      #{order.stripe_checkout_session_object.customer_details.email}
-
-      Телефон:
-      #{order.stripe_checkout_session_object.customer_details.phone}
-    MESSAGE
-
-    Telegrama.send_message(message, disable_web_page_preview: true)
   end
 end
