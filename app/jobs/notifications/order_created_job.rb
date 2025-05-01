1715711f6df5bf34fda8b1f930201544402bdae2
Yaro Shm <yshmarov@gmail.com>
2025-05-01 12:02:17 +0200

    style Notifications::OrderCreatedJob

--- app/jobs/notifications/order_created_job.rb
+++ app/jobs/notifications/order_created_job.rb
@@ -13,28 +13,27 @@ class Notifications::OrderCreatedJob < ApplicationJob
     end
 
     message = <<~MESSAGE
-      New order: #{order.obfuscated_id}
+      🛍️ *New Order: #{order.obfuscated_id}*
+
       #{admin_order_url(order)}
 
-      #{order.order_items.map { |item| "#{item.quantity}x #{item.price.product.name} - #{number_to_currency(item.total_amount / 100.0, unit: currency_code_to_symbol(item.price.stripe_price_object.currency))}" }.join("\n")}
+      *Order Items*
+      #{order.order_items.map { |item| "• #{item.quantity}x #{item.price.product.name} - #{number_to_currency(item.total_amount / 100.0, unit: currency_code_to_symbol(item.price.stripe_price_object.currency))}" }.join("\n")}
 
-      Delivery: #{number_to_currency(order.stripe_checkout_session_object.total_details.amount_shipping.to_f / 100, unit: currency_code_to_symbol(order.currency))}
-      Tax: #{number_to_currency(order.stripe_checkout_session_object.total_details.amount_tax.to_f / 100, unit: currency_code_to_symbol(order.currency))}
-      Discount: #{number_to_currency(-order.stripe_checkout_session_object.total_details.amount_discount.to_f / 100, unit: currency_code_to_symbol(order.currency))}
-      Total: #{number_to_currency(order.total_amount / 100.0, unit: currency_code_to_symbol(order.currency))}
+      *Order Summary*
+      • Delivery: #{number_to_currency(order.stripe_checkout_session_object.total_details.amount_shipping.to_f / 100, unit: currency_code_to_symbol(order.currency))}
+      • Tax: #{number_to_currency(order.stripe_checkout_session_object.total_details.amount_tax.to_f / 100, unit: currency_code_to_symbol(order.currency))}
+      • Discount: #{number_to_currency(-order.stripe_checkout_session_object.total_details.amount_discount.to_f / 100, unit: currency_code_to_symbol(order.currency))}
+      • Total: #{number_to_currency(order.total_amount / 100.0, unit: currency_code_to_symbol(order.currency))}
 
-      Delivery address:
+      *Delivery Address*
       #{order.stripe_checkout_session_object.collected_information.shipping_details.address.values.compact.join(", ")}
 
-      Customer name:
-      #{order.stripe_checkout_session_object.customer_details.name}
-
-      Customer email:
-      #{order.stripe_checkout_session_object.customer_details.email}
-
-      Customer phone:
-      #{order.stripe_checkout_session_object.customer_details.phone}
+      *Customer Details*
+      • Name: #{order.stripe_checkout_session_object.customer_details.name}
+      • Email: #{order.stripe_checkout_session_object.customer_details.email}
+      • Phone: #{order.stripe_checkout_session_object.customer_details.phone}
     MESSAGE
-    Telegrama.send_message(message, disable_web_page_preview: true)
+    Telegrama.send_message(message, parse_mode: "MarkdownV2", disable_web_page_preview: true)
   end
 end
