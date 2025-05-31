654992f2840f6cf43797bef06418863444eee92b
Yaro Shm <yshmarov@gmail.com>
2025-05-31 23:31:38 +0200

    flash transition

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,18 +1,28 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
-  static targets = ["message"]
+  static targets = ['message']
 
   connect() {
+    // Ensure initial styles are applied, then trigger transition
+    requestAnimationFrame(() => {
+      this.element.classList.remove('opacity-0', '-translate-y-full')
+      this.element.classList.add('opacity-100', 'translate-y-0')
+    })
+
     setTimeout(() => {
       this.dismiss()
     }, 5000)
   }
 
   dismiss() {
-    this.element.classList.add("animate-fade-out-right")
+    // Transition to invisible and off-screen
+    this.element.classList.remove('opacity-100', 'translate-y-0')
+    this.element.classList.add('opacity-0', '-translate-y-full')
+
+    // Remove element after transition (duration is 300ms from Tailwind classes)
     setTimeout(() => {
       this.element.remove()
     }, 300)
   }
-} 
+}
