32e22458e332b44ca0d691ecdb63cc188329e7e9
Yaro Shm <yshmarov@gmail.com>
2025-06-01 14:48:55 +0200

    calc subtotal

--- db/migrate/20250508164604_add_subtotal_amount_to_orders.rb
+++ db/migrate/20250508164604_add_subtotal_amount_to_orders.rb
@@ -3,11 +3,8 @@ class AddSubtotalAmountToOrders < ActiveRecord::Migration[8.0]
     add_column :orders, :subtotal_amount, :bigint, null: false, default: 0
 
     Order.find_each do |order|
-      order.update_column(:subtotal_amount, order.total_amount)
-      # calculate new total amount
-      next if order.draft?
-      next if order.stripe_checkout_session_object.nil?
-      order.update_column(:total_amount, order.total_amount + order.stripe_checkout_session_object.total_details.amount_shipping + order.stripe_checkout_session_object.total_details.amount_tax - order.stripe_checkout_session_object.total_details.amount_discount)
+      order.calculate_subtotal_amount
+      order.calculate_total_amount
     end
   end
 end
